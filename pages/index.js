43ad3dd68dfb9d938f29f7aec51d4caed7c09937
antehl <75947069+antehl@users.noreply.github.com>
2021-12-02 22:20:10 +0100

    query departures and display results

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import createEnturService from '@entur/sdk'
-import { useState } from 'react'
+import styles from "../styles/Home.module.css";
+import createEnturService from "@entur/sdk";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [data, setData] = useState([]) 
+	const [data, setData] = useState([]);
 
-  const service = createEnturService({
-    clientName: 'chaIM-customtavle'
-})
+	const service = createEnturService({ clientName: 'chaIM-customtavle' });
 
-service.getDeparturesFromStopPlace('NSR:StopPlace:43916', { //NSR:StopPlace:43916 is Charlottenlund VGS
-  limit: 5, // We are only interested in the next five departures
-  whiteListedModes: ['bus'] // We only care about busses
-})
-.then(departures => {
-    console.log(departures)
-    // setData(departures)
-})
+	const setDepartures = async () => {
+		//NSR:StopPlace:43916 is Charlottenlund VGS
+		let departures = await service.getDeparturesFromStopPlace(
+			'NSR:StopPlace:43916',
+			{ limit: 5 }
+		);
 
-  return (
-    <div className={styles.container}>
-      <h1>Hello world</h1>
-    </div>
-  )
+		setData(departures.map(item=>{
+
+			let formattedTime = new Date(item.expectedArrivalTime).toLocaleTimeString(
+				navigator.language, {hour: '2-digit', minute:'2-digit'}
+			);
+
+			let minuteDiff = (
+				new Date(item.expectedArrivalTime).getTime() - Date.now()
+			) / (1000*60);
+
+			if (minuteDiff < 10) {
+				formattedTime = Math.round(minuteDiff) + " min";
+			}
+
+			return {
+				time: formattedTime,
+				display: item.destinationDisplay.frontText,
+				id: item.serviceJourney.id
+			}
+		}));
+	}
+
+	useEffect(()=>{
+
+		setDepartures();
+		let intervalId = setInterval(setDepartures, 5000);
+
+		return () => { clearInterval(intervalId) };
+
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			{data.map(el=>(
+				<p key={el.id}>
+					{el.time} - {el.display}
+				</p>
+			))}
+		</div>
+	)
 }
