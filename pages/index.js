293e1d706d240590ca591fe65a1cb343ccac02fc
McNobby <76711087+McNobby@users.noreply.github.com>
2021-12-02 15:24:30 +0100

    prepared for useState but spamms the api

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import createEnturService from '@entur/sdk'
+import { useState } from 'react'
 
 export default function Home() {
 
+  const [data, setData] = useState([]) 
+
   const service = createEnturService({
-    // Replace this with your own client name:
-    clientName: 'chamedia-customtavle'
+    clientName: 'chaIM-customtavle'
 })
 
-service.getDeparturesFromStopPlace('NSR:StopPlace:43916', {
+service.getDeparturesFromStopPlace('NSR:StopPlace:43916', { //NSR:StopPlace:43916 is Charlottenlund VGS
   limit: 5, // We are only interested in the next five departures
   whiteListedModes: ['bus'] // We only care about busses
-}).then(departures => console.log(departures))
+})
+.then(departures => {
+    console.log(departures)
+    // setData(departures)
+})
 
   return (
     <div className={styles.container}>
