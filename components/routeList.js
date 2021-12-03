70109c45560423a66605e22babc0d0da4a0e47d8
McNobby <76711087+McNobby@users.noreply.github.com>
2021-12-03 11:10:29 +0100

    Styling and possibility to define custom stops

--- components/routeList.js
+++ components/routeList.js
@@ -2,20 +2,18 @@ import styles from "../styles/Home.module.css";
 import createEnturService from "@entur/sdk";
 import { useEffect, useState } from "react";
 
-export default function RouteList(props) {
-
-    const {service, routeID, limit} = props //destructuring props
+export default function RouteList({ service, routeID, display, limit }) {
 
 	const [data, setData] = useState([]);
-
+    const [stopName, setStopName] = useState('')
+    
 	const setDepartures = async () => {
 		
 		let departures = await service.getDeparturesFromStopPlace(
 			routeID,
 			{ limit }
-		);
-        
-		setData(departures.map(item=>{
+		);  
+		let newData = departures.map(item=>{
 
 			let formattedTime = new Date(item.expectedArrivalTime).toLocaleTimeString(
 				navigator.language, {hour: '2-digit', minute:'2-digit'}
@@ -27,32 +25,67 @@ export default function RouteList(props) {
 
 			if (minuteDiff < 10) {
 				formattedTime = Math.round(minuteDiff) + " min";
-				if (Math.round(minuteDiff) === 0) formattedTime = "nå";
+				if (Math.round(minuteDiff) <= 0) formattedTime = "Nå";
 			}
+
+			// if (item.destinationDisplay.frontText !== display) return;
 
 			return {
 				time: formattedTime,
-				display: item.destinationDisplay.frontText,
-				id: item.serviceJourney.id
+				display: (!display && ` - ${item.destinationDisplay.frontText}`),
+				id: item.serviceJourney.id,
+                quay: item.quay.publicCode
 			}
-		}));
+		});
+
+		setData(newData);//.filter(e=>e));
 	}
 
-	useEffect(()=>{
+    const getStopName = async () => {
+        const stopPlace = await service.nsr.getStopPlace(
+            routeID,
+        )
+        setStopName(stopPlace.name.value)
+    }
 
+	useEffect(()=>{
+        getStopName();
 		setDepartures();
 		let intervalId = setInterval(setDepartures, 5000);
-
+        
 		return () => { clearInterval(intervalId) };
 
 	}, []);
 
+    const textColor = (time) => {
+
+        if(time.startsWith('Nå')){
+            return(
+                {color:"#ebc354"}
+            )
+        }
+
+        if(time[2] !== 'm'){
+            return
+        }
+
+        if(parseInt(time[0]) < 6){
+            return(
+                {color:"#ebc354"}
+            )
+        }
+    }
+
 	return (
-		<div className={styles.container}>
+		<div className={styles.timeTable}>
+            <h2>{stopName}</h2>
 			{data.map(el=>(
-				<p key={el.id}>
-					{el.time} - {el.display}
-				</p>
+                <div className={styles.time} key={el.id} style={textColor(el.time)} >
+				    <h3 >
+				    	{el.time}{el.display}
+				    </h3>
+                    <p>{el.quay && `(Stopp ${el.quay})`}</p>
+                </div>
 			))}
 		</div>
 	)
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
 import styles from "../styles/Home.module.css";
 import createEnturService from "@entur/sdk";
 import { useEffect, useState } from "react";
-import RouteList from "../components/routeList";
+import RouteList from "../components/RouteList";
 
 export default function Home() {
 
+  const [stops, setStops] = useState([])
 
 	const service = createEnturService({ clientName: 'chaIM-customtavle' });
 
+useEffect(()=> {
+	if(!localStorage.getItem("stops")) { //if there is no saved stops in browser
+    let savedStops = []
+    for (let i = 0; i < 3; i++) {
+      let stop = prompt("sett inn NSR id for stoppplasser (https://stoppested.entur.org/) skriv ferdig om du er ferdig")
+      if(stop.toLowerCase() === 'ferdig'){
+        break
+      }
+      savedStops.push(`NSR:StopPlace:${stop}`)
+    }
+
+		localStorage.setItem("stops", JSON.stringify(savedStops))
+	}
+
+  const stopsInBrowser = JSON.parse(localStorage.getItem("stops"))
+  setStops(stopsInBrowser)
+},[])
+
+//to find stopplace id visit: https://stoppested.entur.org/ 
 	return (
-		<div className={styles.container}>
-      <RouteList service={service} routeID="NSR:StopPlace:43916" limit={10} />
+		<div className={styles.mainPage}>
+      {stops.map(e=>(
+        <RouteList service={service} routeID={e} limit={10} />
+      ))}
 		</div>
 	)
 }
