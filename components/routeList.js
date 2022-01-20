2faeb1954cf28a57a3ad4e3201445c11578bb8b1
antehl <75947069+antehl@users.noreply.github.com>
2022-01-20 12:59:39 +0100

    faster loading (potentially), less fetch spam
    makes the page fetch entur's stops once per 30 seconds instead of 5 (gets bus expected time delays). page now only refreshes stop times every 5 seconds

--- components/routeList.js
+++ components/routeList.js
@@ -6,27 +6,29 @@ export default function RouteList({ service, routeID, limit }) {
 
 	const router = useRouter();
 
-	const [data, setData] = useState([]);
 	const [stopName, setStopName] = useState('');
-	
-	//this function runs every 5 seconds. Is called in the useEffect hook
-	//this function gets time tables for a selected stop by NSR ID
-	const setDepartures = async () => {
-		//fetches from entur api
-		let departures = await service.getDeparturesFromStopPlace(
+	const [timeList, setTimeList] = useState([]);
+
+	const fetchDepartures = async () => {
+		return await service.getDeparturesFromStopPlace(
 			routeID,
 			{ limit }
 		);
+	}
+	
+	//this function runs every 5 seconds. Is called in the useEffect hook
+	//this function gets time tables for a selected stop by NSR ID
+	const displayDepartures = list => {
 
-		setData(departures.map(item => {
+		const data = list.map(item => {
 
 			// format time to hh:mm
-			let formattedTime = new Date(item.expectedArrivalTime).toLocaleTimeString(
+			const formattedTime = new Date(item.expectedArrivalTime).toLocaleTimeString(
 				navigator.language, {hour: '2-digit', minute:'2-digit'}
 			);
 
 			// difference between now and bus arrival time
-			let minuteDiff = (
+			const minuteDiff = (
 				new Date(item.expectedArrivalTime).getTime() - Date.now()
 			) / (1000*60);
 
@@ -43,7 +45,16 @@ export default function RouteList({ service, routeID, limit }) {
 				id: item.serviceJourney.id,
 				quay: item.quay.publicCode
 			}
-		}));
+		});
+
+		setTimeList(data.map(el => (
+			<div className={styles.time} key={el.id} style={textColor(el.time)} >
+				<h3 >
+					{el.time} - {el.display}
+				</h3>
+				<p>{el.quay && `(Stopp ${el.quay})`}</p>
+			</div>
+		)));
 	}
 	//this runs when the component loads (called in use effect hook )
 	//it gets the stopname of the selected stop by NSR ID
@@ -55,14 +66,25 @@ export default function RouteList({ service, routeID, limit }) {
 		setStopName(stopPlace.name.value)
 	}
 
-	useEffect(()=>{
-		getStopName();
-		setDepartures();
-		// update departures per 5 sec
-		let intervalId = setInterval(setDepartures, 5000);
-		// free interval
-		return () => { clearInterval(intervalId) };
+	useEffect(() => {
 
+		let departureList;
+
+		const _fetch = async () => { departureList = await fetchDepartures() }
+		const _display = () => { displayDepartures(departureList) }
+
+		getStopName();
+
+		(async () => {
+			await _fetch();
+			_display();
+		})();
+
+		let fetchIntervalId = setInterval(_fetch, 30000);
+		// update departures per 5 sec
+		let displayIntervalId = setInterval(_display, 5000);
+		// free interval
+		return () => { clearInterval(fetchIntervalId); clearInterval(displayIntervalId) };
 	}, []);
 
 	//this function returns a css value making text yellow if its five minutes or less left to arrival
@@ -81,23 +103,9 @@ export default function RouteList({ service, routeID, limit }) {
 		}
 	}
 
-	const timeList = data.map(el => {
-		return(
-				<div className={styles.time} key={el.id} style={textColor(el.time)} >
-					<h3 >
-						{el.time} - {el.display}
-					</h3>
-					<p>{el.quay && `(Stopp ${el.quay})`}</p>
-				</div>
-			)
-		})
-	const edit = () => {
-		router.push("/edit");
-	}
-			
 	return (
 		<div className={styles.timeTable}>
-			<h2 onClick={edit}>{stopName}</h2>
+			<h2 onClick={()=>{ router.push("/edit") }}>{stopName}</h2>
 			{timeList}
 		</div>
 	)
